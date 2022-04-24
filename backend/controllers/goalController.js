a530249cce5b3ffddd7b96d9dc719504d94d9372
Wyvern <75375077+the0nlyWyvern@users.noreply.github.com>
2022-04-24 22:03:12 +0700

    finish 2nd video

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 // @desc Get goals
 // @route GET/api/goals
 // @access Private
 const getGoals = asyncHandler(async(req, res) => {
-    const goals = await Goal.find()
+    const goals = await Goal.find({user: req.user.id})
     res.status(200).json(goals)
 })
 
@@ -19,7 +20,8 @@ const setGoal = asyncHandler(async(req, res) => {
         throw new Error('Please add a text field')
     }
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id,
     })
 
     res.status(200).json(goal)
@@ -33,6 +35,20 @@ const updateGoal = asyncHandler(async(req, res) => {
     if (!goal){
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    // Check for user
+    if (!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User & goal might be different')
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -49,7 +65,21 @@ const deleteGoal = asyncHandler(async(req, res) => {
     const goal = await Goal.findById(req.params.id)
     if (!goal){
         res.status(400)
-        throw new Error('Goal not found')
+        throw new Error('Goal not found.')
+    }
+
+    const user = await User.findById(req.user.id)
+
+    // Check for user
+    if (!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User & goal might be different')
     }
 
     await goal.remove()
@@ -61,5 +91,5 @@ module.exports = {
     getGoals, 
     setGoal,
     updateGoal,
-    deleteGoal
+    deleteGoal,
 }
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,25 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const {getGoals, 
-    setGoal, 
-    deleteGoal, 
-    updateGoal
+    setGoal,  
+    updateGoal,
+    deleteGoal,
 } = require('../controllers/goalController')
 
-// router.get('/', getGoals)
-// router.post('/', setGoal)
-router.route('/').get(getGoals).post(setGoal)
+const {protect} = require('../middleware/authMiddleware')
 
-
-
-// router.put('/:id', updateGoal)
-
-// router.delete('/:id', (req, res) => {
-//     res.status(200).json({ message: `Delete goal ${req.params.id}`})
-// })
-//router.delete('/:id', deleteGoal)
-
-router.route('/:id').delete(deleteGoal).put(updateGoal)
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
 
 
 module.exports = router
